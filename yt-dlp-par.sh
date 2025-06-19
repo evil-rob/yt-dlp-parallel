a3e6f18671fb34748efdd8bc1bc0275fd9d55a6c
Robert Blank <evilrob@lunarjockey.com>
2025-06-19 11:08:26 -0700

    Implemented process managment.
    Main process will hang out in a loop when max jobs are running. While in
    the loop, it will poll each PID and wait on them as they terminate.
    After waiting on a terminated PID, it will remove a PID from the list so
    the next URL in the list can download.

--- yt-dlp-par.sh
+++ yt-dlp-par.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 yt_command="/usr/bin/yt-dlp"
+max_jobs="4"
 
 trap 'echo "Ctrl-C caught"; killall yt-dlp; exit 1' SIGINT
 
 echo "<<PID $$ started>> [Main Process]"
 
-while [ $# -gt 0 ]
+for url in "$@"
 do
-    url="$1"
     opts="--no-progress"
 
     # Spawn a background process to download the URL.
     # Keep track of each PID in a list. Spaces will be the delimiter.
     $yt_command $opts "$url" &
     echo "<<PID $! started>>"
-    [ -z "${pids+x}" ] && pids="$!" || pids="$pids $!"
+    pids="$pids $!"
 
     # Save only YouTube URLs into a list so they will be marked as played.
     # Before the first URL to be saved, the list is not initialiazed. Just
@@ -26,9 +26,38 @@ do
         [ -z "${to_mark+x}" ] && to_mark="$url" || to_mark=$(printf "%s\n%s" "$to_mark" "$url")
     fi
 
-    # Next argument will have the next link.
-    shift
-    sleep 1
+    # Sit in the while loop when there are $max_jobs PIDs running. Keep
+    # polling the PIDs list and wait on each PID that terminates. Use kill -0
+    # to check if a PID has terminated. After waiting on a PID, remove it from
+    # the PIDs list.
+    while [ $(echo "$pids" | wc -w) -ge "$max_jobs" ]
+    do
+        # Poll each PID
+        for pid in $pids
+        do
+            # Check if PID has terminated
+            if ! kill -0 "$pid" 2>/dev/null
+            then 
+                # Wait on the PID that terminated
+                if wait "$pid"
+                then
+                    echo "[$0] $pid exited normally."
+                else
+                    echo "[$0] $pid exited with errors."
+                fi
+
+                # Remove PID from the PIDs list by iterating through the old
+                # list and skipping over the one that terminated.
+                for p in $pids
+                do
+                    [ "$p" != "$pid" ] && new_pids="$new_pids $p"
+                done
+                pids="$new_pids"
+                unset new_pids
+            fi
+        done
+        sleep 1
+    done
 done
 
 # Mark all URLs in $to_mark as watched. Each URL in the list is on a line.
